a97042e83ee8f31ae572616fdb50cc2d6096e46a
Paul Hadfield <paul@phad.org.uk>
2017-10-30 17:19:39 +0000

    Add basics of hledger serialization to text output file.

---
+++ converter/convert.go
@@ -0,0 +1,32 @@
+package converter
+
+import (
+	"github.com/phad/msmtohl/model"
+	"github.com/phad/msmtohl/parser/qif"
+)
+
+func FromQIF(rs *qif.RecordSet) ([]*model.Transaction, error) {
+	var txns []*model.Transaction
+	for _, r := range rs.Records {
+		d, err := qif.ParseDate(r.Date)
+		if err != nil {
+			return nil, err
+		}
+		txns = append(txns, &model.Transaction{
+			Date: d,
+			Status: fromQIFStatus(r.Cleared),
+			Payee: r.Payee,
+			Description: r.Memo,
+		})
+	}
+	return txns, nil
+}
+
+func fromQIFStatus(qs string) model.Status {
+	switch qs {
+	case " ": return model.Unmarked
+	case "*", "C": return model.Pending
+	case "X", "R": return model.Cleared
+	}
+	return model.Unknown
+}
--- converter/main/main.go
+++ converter/main/main.go
@@ -6,10 +6,12 @@ import (
 	"log"
 	"os"
 
+	"github.com/phad/msmtohl/converter"
 	"github.com/phad/msmtohl/parser/qif"
 )
 
 var inFile = flag.String("in_file", "", "Input file in QIF format.")
+var outFile = flag.String("out_file", "", "Output file in hledger format.")
 
 func main() {
 	flag.Parse()
@@ -18,13 +20,31 @@ func main() {
 
 	qf, err := os.Open(*inFile)
 	if err != nil {
-		panic(fmt.Errorf("QIF error: %v", err))
+		panic(fmt.Errorf("os.File error: %v", err))
 	}
 	defer qf.Close()
+
+	hlf, err := os.Create(*outFile)
+	if err != nil {
+		panic(fmt.Errorf("os.File error: %v", err))
+	}
+	defer hlf.Close()
 
 	rs, err := qif.NewRecordSet(qf)
 	if err != nil {
 		log.Fatalf("Reading file %q got error: %v", *inFile, err)
 	}
 	log.Printf("For account %q read %d records.", rs.AccountName(), len(rs.Records))
+
+	txns, err := converter.FromQIF(rs)
+	if err != nil {
+		log.Fatalf("Converting QIF RecordSet got error: %v", err)
+	}
+	log.Printf("Converted %d records.", len(txns))
+
+	for i := 0; i < 10; i++ {
+		if err = txns[i].SerializeHledger(hlf); err != nil {
+			panic(err)
+		}
+	}
 }
