5e5ce9022f3c3658e139a9234967172806291d45
Paul Hadfield <paul@phad.org.uk>
2017-10-31 11:01:32 +0000

    Add converter tests.

--- converter/convert.go
+++ converter/convert.go
@@ -10,18 +10,26 @@ import (
 func FromQIF(rs *qif.RecordSet) ([]*model.Transaction, error) {
 	var txns []*model.Transaction
 	for _, r := range rs.Records {
-		d, err := qif.ParseDate(r.Date)
+		t, err := fromQIFRecord(r)
 		if err != nil {
 			return nil, err
 		}
-		txns = append(txns, &model.Transaction{
-			Date:        d,
-			Status:      fromQIFStatus(r.Cleared),
-			Payee:       r.Payee,
-			Description: r.Memo,
-		})
+		txns = append(txns, t)
 	}
 	return txns, nil
+}
+
+func fromQIFRecord(r *qif.Record) (*model.Transaction, error) {
+	d, err := qif.ParseDate(r.Date)
+	if err != nil {
+		return nil, err
+	}
+	return &model.Transaction{
+		Date:        d,
+		Status:      fromQIFStatus(r.Cleared),
+		Payee:       r.Payee,
+		Description: r.Memo,
+	}, nil	
 }
 
 func fromQIFStatus(qs string) model.Status {
---
+++ converter/convert_test.go
@@ -0,0 +1,88 @@
+package converter
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/phad/msmtohl/model"
+	"github.com/phad/msmtohl/parser/qif"
+)
+
+func TestFromQIFRecord(t *testing.T) {
+	tests := []struct{
+		desc string
+		qifRec *qif.Record
+		want *model.Transaction
+		wantErr bool
+	}{
+		{
+			desc: "mangled date",
+			qifRec: &qif.Record{
+				Date: "12'02/2016",
+				Cleared: "C",
+				Payee: "Dave",
+				Memo: "New shoes",
+			},
+			wantErr: true,
+		},
+		{
+			desc: "all supported fields present",
+			qifRec: &qif.Record{
+				Date: "12/02'2016",
+				Cleared: "C",
+				Payee: "Dave",
+				Memo: "New shoes",
+			},
+			want: &model.Transaction{
+				Date:        time.Date(2016, time.February, 12, 0, 0, 0, 0, time.UTC),
+				Status:      model.Pending,
+				Payee:       "Dave",
+				Description: "New shoes",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			txn, err := fromQIFRecord(test.qifRec)
+			if gotErr := err != nil; gotErr != test.wantErr {
+				t.Errorf("fromQIFRecord()=_, err? %t want? %t (err=%v)", gotErr, test.wantErr, err)
+			}
+			if !reflect.DeepEqual(txn, test.want) {
+				t.Errorf("fromQIFRecord()=%v want %v", txn, test.want)
+			}
+		})
+	}	
+}
+
+func TestFromQIFStatus(t *testing.T) {
+	tests := []struct{
+		inputs []string
+		want model.Status
+	}{
+		{
+			inputs: []string{"", "A", "blah"},
+			want: model.Unknown,
+		},
+		{
+			inputs: []string{" "},
+			want: model.Unmarked,
+		},
+		{
+			inputs: []string{"*", "C"},
+			want: model.Pending,
+		},
+		{
+			inputs: []string{"R", "X"},
+			want: model.Cleared,
+		},
+	}
+	for _, test := range tests {
+		for _, in := range test.inputs {
+			if got, want := fromQIFStatus(in), test.want; got != want {
+				t.Errorf("fromQIFStatus(%q)=%v want %v", in, got, want)
+			}			
+		}
+	}
+}
